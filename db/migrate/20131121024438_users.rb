042dc5c079d347ba274402c65c5d71ba648583bb
OhMedrano <Omedrano564@gmail.com>
2013-11-20 21:48:33 -0500

    new

---
+++ db/migrate/20131121024438_users.rb
@@ -0,0 +1,15 @@
+class Users < ActiveRecord::Migration
+  def up
+  	create_table :users do |t|
+  		t.string :fname
+  		t.string :lname
+  		t.string :email
+  		t.string :password_hash
+  		t.string :password_salt
+  	end
+  end
+
+  def down
+  	drop_table :users
+  end
+end
